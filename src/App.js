4e237c6aacd523872224a393a79cddf933eba558
Jose Sotolongo <joseesotolongo@gmail.com>
2023-11-30 21:49:01 -0500

    Changed text color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold underline'>
+      <h1 className='text-3xl font-bold underline text-teal-600'>
         Hello world!
       </h1>
     </div>
