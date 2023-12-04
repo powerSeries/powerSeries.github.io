8b2639c50983996d91d55285c68c6315486234ae
Jose Sotolongo <joseesotolongo@gmail.com>
2023-12-03 23:00:52 -0500

    Updated navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <nav className="flex sm:justify-center space-x-4">
-    {[
-      ['Home', '/'],
-      ['Project', '/project'],
-      ['Contact', '/contact'],
-    ].map(([title, url]) => (
-      <Link key={title} to={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</Link>
-    ))}
-</nav>
+  return (
+    <nav className="flex sm:justify-center space-x-4 shadow-sm bg-zinc-800">
+      <h1 className="px-3 py-2 flex sm:justify-start space-x-4 text-white">Jose Sotolongo</h1>
+      {[
+        ["Home", "/"],
+        ["Project", "/project"],
+        ["Contact", "/contact"],
+      ].map(([title, url]) => (
+        <Link
+          key={title}
+          to={url}
+          className="px-3 py-2 text-white font-medium hover:bg-zinc-500 hover:text-slate-900"
+        >
+          {title}
+        </Link>
+      ))}
+    </nav>
+  );
 }
