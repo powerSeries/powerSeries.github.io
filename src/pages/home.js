d430b149822e8f0f4b8ad9128e2b249e0526e265
Jose Sotolongo <joseesotolongo@gmail.com>
2023-12-05 19:49:54 -0500

    Added profile photo and started styling home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,23 @@
+import profile_photo from '../assets/gojo.jfif';
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-teal-600">
+    <div className="grid grid-rows-3 grid-flow-col justify-items-center items-center">
+      <div className="bg-gray-200 border-2 h-fit w-full flex flex-wrap content-around grid grid-cols-2">
+        <div className="border-2 text-black">
+          <h1 className="text-5xl">Home</h1>
+          <p>This is a paragrah tag why is the header not bold</p>
+        </div>
+        <div className="border-2 text-black">
+          <img src={profile_photo} alt="ProfilePhoto"/>
+        </div>
+      </div>
+      <div className="bg-gray-500 border-2 h-auto w-full flex flex-wrap content-around">
         Home
-      </h1>
+      </div>
+      <div className="bg-gray-700 text-white h-fit w-full">
+        Home
+      </div>
     </div>
   );
 }
